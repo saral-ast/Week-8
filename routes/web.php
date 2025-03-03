37cfc46eb3963e2a02f7dccb8c20d09b032cb4cf
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-03-03 15:08:10 +0530

    use resource to controle the routes

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,16 @@ use App\Http\Controllers\JobController;
 Route::view("/","home",["name"=> "Saral Chauhan"]);
 Route::view("/about","about");
 Route::view("/contact","contact");
+Route::resource("/jobs", JobController::class);
 
-Route::controller(JobController::class)->group(function(){
-    Route::get("/jobs","index");
-    Route::get("/jobs/create","create");
-    Route::get("/jobs/{job}","show");
-    Route::get("/jobs/{job}/edit","edit");
-    Route::post("/jobs","store");
-    Route::patch("/jobs/{job}","update");
-    Route::delete("/jobs/{job}","destroy");
+// Route::controller(JobController::class)->group(function(){
+//     Route::get("/jobs","index");
+//     Route::get("/jobs/create","create");
+//     Route::get("/jobs/{job}","show");
+//     Route::get("/jobs/{job}/edit","edit");
+//     Route::post("/jobs","store");
+//     Route::patch("/jobs/{job}","update");
+//     Route::delete("/jobs/{job}","destroy");
     
-});
+// });
+
